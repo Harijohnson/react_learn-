7ff72a6d97388671e89a7968d5d0314e8e686ceb
Harijohnson <harijn72@gmail.com>
2023-11-17 22:53:36 +0530

    wrod changes in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
-import  Test  from "./component/Sample"
-
-
 function App(){
-  // let name = "Hari"
-  return(
+  function newWords(){
+    let words = [ 'trail and error ' , 'slow way' , 'chat gpt ' , 'stack overflow']
+    let int  = Math.floor(Math.random()*4)
+    return words[int]
+  }
 
-    
-    // <div className='App'>
-    //  <h1> Welcome to react first page </h1>
-    //   <h1> {name} </h1>
-    // </div>
+  return(
     <>
-      <h1> Sample component </h1>
-      <Test></Test>
+    Learn react
+
+    <p> lets think { newWords() } to leran react first time using proejct</p>
+    
+    
+    
     </>
   )
 }
