2dd4d4e3506d22693054667f2f808117c79fe9d3
Harijohnson <harijn72@gmail.com>
2023-11-17 21:35:29 +0530

    component is creted in jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
+import  Test  from "./component/Sample"
+
+
 function App(){
-  let name = "Hari"
+  // let name = "Hari"
   return(
 
     
-    <div className='App'>
-     <h1> Welcome to react first pagr </h1>
-      <h1> {name} </h1>
-    </div>
+    // <div className='App'>
+    //  <h1> Welcome to react first page </h1>
+    //   <h1> {name} </h1>
+    // </div>
+    <>
+      <h1> Sample component </h1>
+      <Test></Test>
+    </>
   )
 }
 
 
-export default App
+export default App;
---
+++ frontend/src/component/Sample.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Test(){
+    return (
+        <div className="Sample">
+            <h1>Welcome to component</h1>
+             {/* <Test></Test>   returning the same file */}
+        </div>
+    );
+}
+
+
+export default Test;
